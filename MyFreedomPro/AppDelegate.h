c8b8f0a90312ae1fb94ac08d890152312026aa33
HLL <hllfj922@163.com>
2016-09-28 09:44:19 +0800

    Initial Commit

---
+++ MyFreedomPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFreedomPro
+//
+//  Created by Apple on 16/9/28.
+//  Copyright © 2016年 亮哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
